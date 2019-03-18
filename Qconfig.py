ad9c59bba0671db7c40cb96d2ceef813dd05b511
SaltyJahmale <theebag77@gmail.com>
2019-03-18 20:42:39 +0100

    Delete Qconfig.py
    Key

--- Qconfig.py
+++
@@ -1,5 +0,0 @@
-APItoken = '7e421ed6b167dbf048f147288a52dbec39b84833097f680d20ecae2f4e7ffc4aa7507d26f0b51bf4cd6d49594e64d894b6abe7cf41e3b0f0d83348b2bf952bab'
-
-config = {
-    'url': 'https://quantumexperience.ng.bluemix.net/api'
-}
